fc0f82eaefc4d9e640d6155e7d2021b760d374dd
Samer Masterson <samer@samertm.com>
2014-08-27 20:01:40 -0400

    TODO regexp now matches "TODO(username):"

--- exps.go
+++ exps.go
@@ -2,7 +2,7 @@ package main
 
 const (
 	ISSUE_URL_REGEX = "\\[(Issue:[^\\]]*)\\]"
-	TODO_REGEX      = "TODO:(.*)" //TODO: Ignore line [Issue: solved]
+	TODO_REGEX      = "TODO(\\([^)]+\\))?:(.*)"
 	ISSUE_BODY      = "On file: [%s](%s)"
 	GITHUB_FILE_URL = "https://github.com/%s/%s/blob/%s/%s"
 )
